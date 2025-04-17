386d9c3697297bc1d7d830bc4d43c5680e65a81a
Hipjea <pierre.duverneix@gmail.com>
2025-04-17 17:48:23 +0200

    readme & structure

---
+++ README.md
@@ -0,0 +1,22 @@
+# Hangman
+
+## Setup
+
+Setup the ALR toolchain:
+
+```sh
+alr toolchain --select
+alr settings --set --global editor.cmd "code ."
+```
+
+Create a new project:
+
+```sh
+alr init --bin hangman
+```
+
+Run the project:
+
+```sh
+alr run
+```
--- src/hangman.adb
+++ src/hangman.adb
@@ -1,21 +1,35 @@
-with Ada.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Hangman is
-   -- Declare the function
-   function Get_Name return String;
+   Input_Max_Length : constant Positive := 100;
 
-   -- Define the function
+   -- Functions declarations
+   function Get_Name return String;
+   function Set_Secret_Word return String;
+
+   -- Functions definitions
    function Get_Name return String is
-      Input  : String (1 .. 100);
+      Input  : String (1 .. Input_Max_Length);
       Length : Natural;
    begin
-      Ada.Text_IO.Get_Line (Input, Length);
+      Put_Line ("What's your name?");
+      Get_Line (Input, Length);
       return Input (1 .. Length);
    end Get_Name;
 
-   -- Declare the variable and assign its value from the function return value
-   Name : constant String := Get_Name;
+   function Set_Secret_Word return String is
+      Input  : String (1 .. Input_Max_Length);
+      Length : Natural;
+   begin
+      Put_Line ("Write the secret word to guess:");
+      Get_Line (Input, Length);
+      return Input (1 .. Length);
+   end Set_Secret_Word;
 
+   -- Variables declarations & value assignment
+   Name       : constant String := Get_Name;
+   SecretWord : constant String := Set_Secret_Word;
 begin
-   Ada.Text_IO.Put_Line ("Hello, " & Name & "!");
+   Put_Line ("Hello, " & Name & "!");
+   Put_Line ("The secret word is: " & SecretWord);
 end Hangman;
